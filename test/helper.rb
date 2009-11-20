3d9fe4cd11049d3abc98755434c7b94771aada90
Javier Goizueta <jgoizueta@gmail.com>
2009-11-20 10:55:09 +0100

    Fix test helper.

--- test/helper.rb
+++ test/helper.rb
@@ -1,8 +1,10 @@
-require 'helper'
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'Geo2D'
+require 'geo2d'
 
 class Test::Unit::TestCase
 end
